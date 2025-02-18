8a4ece71712dd2b8a59ccc5c6b542008fb97958b
Saiteja Labba <saitejalabba@gmail.com>
2025-02-18 00:23:59 -0600

    Part A

---
+++ Day2A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Day2A {
+    public static void main(String[] args) {
+        int a ;
+        System.out.println("selected for honeywell");
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        System.out.println("You have entered " +a);
+    }
+}
