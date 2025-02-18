c49a7cee8249aef33b062d4d6bbc68d8751e922e
Saiteja Labba <saitejalabba@gmail.com>
2025-02-17 23:51:05 -0600

    Day2

---
+++ Day2.java
@@ -0,0 +1,10 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int a = 19;
+        char c = 'v';
+        System.out.println(a);
+        System.out.print(c);
+        System.out.println("Rockyy Bhai");
+        System.out.println("variable is "+a);
+    }
+}
