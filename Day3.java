b8086e02341187b1f470772e288629d93a10a6ef
Saiteja Labba <saitejalabba@gmail.com>
2025-02-21 13:49:41 -0600

    Day 3

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Day3 {
+    public static void main(String[] args){
+        int a = 6;
+        int b = 2;
+        int max;
+        System.out.println("Welcome to World of Operators");
+        System.out.println((a!=b)||(a==b));//logical operators
+        max = a>b?a:b;
+        System.out.println(a<<b);
+        System.out.println("max value is "+max);
+    }
+}
