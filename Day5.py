83c86d9f0631352fe650fea6d1f9334df21f15ad
Saiteja Labba <saitejalabba@gmail.com>
2025-03-19 08:06:58 -0500

    Python Basics

---
+++ Day5.py
@@ -0,0 +1,14 @@
+# l = int(input("enter length"))
+# b = int(input("enter breadth"))
+
+# area = l*b
+# print(area)
+
+
+l,b = input().split()
+l = int(l)
+b = int(b)
+
+area = l*b
+
+print(area)
---
+++ Day6.py
@@ -0,0 +1,4 @@
+name = input()
+age =  int(input())
+
+print('The name of person is ',name,'and the age is ',age,'.',sep='')
